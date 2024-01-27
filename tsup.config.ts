0adbdc371ab2bc3c42c2089e7452cf1e811cdd06
Alexandre Marques <pt.alexandremarques@gmail.com>
2024-01-27 19:11:15 +0000

    chore: final touches in building process

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig([
-  // Main/common entry
   {
-    entry: [
-      "src/index.ts"
-    ],
+    entry: {
+      "index": "src/index.ts",
+      "json-adapter": "src/lib/adapters/json-adapter/index.ts",
+      "env-adapter": "src/lib/adapters/env-adapter/index.ts",
+    },
     format: ["cjs", "esm"],
     target: "node14", // support Node.js 14 and above
     splitting: false,
@@ -14,32 +15,5 @@ export default defineConfig([
     minify: true,
     dts: true,
     outDir: "dist",
-  },
-  // Adapters
-  {
-    entry: [
-      "src/lib/adapters/json-adapter/index.ts",
-    ],
-    format: ["cjs", "esm"],
-    target: "node14", // support Node.js 14 and above
-    splitting: false,
-    sourcemap: true,
-    clean: true,
-    minify: true,
-    dts: true,
-    outDir: "json-adapter/",
-  },
-  {
-    entry: [
-      "src/lib/adapters/env-adapter/index.ts",
-    ],
-    format: ["cjs", "esm"],
-    target: "node14", // support Node.js 14 and above
-    splitting: false,
-    sourcemap: true,
-    clean: true,
-    minify: true,
-    dts: true,
-    outDir: "env-adapter/",
-  },
+  }
 ]);
