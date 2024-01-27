c4de3dc2a3038dc27c0a74f8d44004c69ffa1dd7
Alexandre Marques <pt.alexandremarques@gmail.com>
2024-01-27 17:24:46 +0000

    chore: change build process

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,17 +1,45 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: [
-    "src/index.ts",
-    "src/lib/adapters/json-adapter/index.ts",
-    "src/lib/adapters/env-adapter/index.ts",
-  ],
-  format: ["cjs", "esm"],
-  target: "node14", // support Node.js 14 and above
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  minify: true,
-  dts: true,
-  outDir: "dist",
-});
+export default defineConfig([
+  // Main/common entry
+  {
+    entry: [
+      "src/index.ts"
+    ],
+    format: ["cjs", "esm"],
+    target: "node14", // support Node.js 14 and above
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    minify: true,
+    dts: true,
+    outDir: "dist",
+  },
+  // Adapters
+  {
+    entry: [
+      "src/lib/adapters/json-adapter/index.ts",
+    ],
+    format: ["cjs", "esm"],
+    target: "node14", // support Node.js 14 and above
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    minify: true,
+    dts: true,
+    outDir: "json-adapter/dist",
+  },
+  {
+    entry: [
+      "src/lib/adapters/env-adapter/index.ts",
+    ],
+    format: ["cjs", "esm"],
+    target: "node14", // support Node.js 14 and above
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    minify: true,
+    dts: true,
+    outDir: "env-adapter/dist",
+  },
+]);
