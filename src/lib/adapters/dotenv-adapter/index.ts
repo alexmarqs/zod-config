97e31d024afec3e06a0bf8993889317d120e7518
Alexandre Marques <pt.alexandremarques@gmail.com>
2024-02-04 17:51:32 +0000

    chore: fix ignored adapters path

---
+++ src/lib/adapters/dotenv-adapter/index.ts
@@ -0,0 +1,36 @@
+import { parse } from "dotenv";
+import { readFile } from "fs/promises";
+import { Adapter } from "../../../types";
+import { filterByPrefixKey } from "../utils";
+
+export type DotEnvAdapterProps = {
+  path: string;
+  prefixKey?: string;
+};
+
+const ADAPTER_NAME = "dotenv adapter";
+
+export const dotEnvAdapter = ({ path, prefixKey }: DotEnvAdapterProps): Adapter => {
+  return {
+    name: ADAPTER_NAME,
+    read: async () => {
+      try {
+        const data = await readFile(path, "utf-8");
+
+        const parsedData = parse(data) || {};
+
+        if (prefixKey) {
+          return filterByPrefixKey(parsedData, prefixKey);
+        }
+
+        return parsedData;
+      } catch (error) {
+        throw new Error(
+          `Failed to parse / read .env file at ${path}: ${
+            error instanceof Error ? error.message : error
+          }`,
+        );
+      }
+    },
+  };
+};
