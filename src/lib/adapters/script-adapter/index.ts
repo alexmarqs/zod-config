2e60893bacded395c476d6a9648c543948447038
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-06-27 21:51:03 +0100

    add javascript/typescript config file adapter

---
+++ src/lib/adapters/script-adapter/index.ts
@@ -0,0 +1,34 @@
+import type { Adapter } from "../../../types";
+import { filterByPrefixKey } from "../utils";
+
+export type ScriptAdapterProps = {
+  path: string;
+  prefixKey?: string;
+  silent?: boolean;
+};
+
+const ADAPTER_NAME = "script adapter";
+
+export const scriptAdapter = ({ path, prefixKey, silent }: ScriptAdapterProps): Adapter => {
+  return {
+    name: ADAPTER_NAME,
+    read: async () => {
+      try {
+        const data = await import(path);
+
+        if (prefixKey) {
+          return filterByPrefixKey(data, prefixKey);
+        }
+
+        return data;
+      } catch (error) {
+        throw new Error(
+          `Failed to import() script at ${path}: ${
+            error instanceof Error ? error.message : error
+          }`,
+        );
+      }
+    },
+    silent,
+  };
+};
