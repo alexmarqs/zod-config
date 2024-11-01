fbf8b6631a530ea348c5214d6c6b118746e8b50d
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-11-01 13:39:56 +0000

    fix: #15

--- src/lib/adapters/utils.ts
+++ src/lib/adapters/utils.ts
@@ -46,6 +46,12 @@ export function deepMerge(target: any, ...sources: any[]) {
   return deepMerge(target, ...sources);
 }
 
-export function isMergeableObject(item: any) {
-  return item && typeof item === "object" && !Array.isArray(item);
+export function isMergeableObject(item: unknown): item is Record<string, unknown> {
+  if (!item) return false
+  if (typeof item !== "object") return false
+  // ES6 class instances, Maps, Sets, Arrays, etc. are not considered records
+  if (Object.getPrototypeOf(item) === Object.prototype) return true
+  // Some library/Node.js functions return records with null prototype
+  if (Object.getPrototypeOf(item) === null) return true
+  return false
 }
